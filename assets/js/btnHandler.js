2898abe6ef28c733d7c1551a02c9623145e4a990
Zephinax <Zephinax@gmail.com>
2024-03-09 11:39:00 +0330

    fix mobile view

--- assets/js/btnHandler.js
+++ assets/js/btnHandler.js
@@ -1,19 +1,32 @@
 let btn = document.getElementById('btn');
 let clickCount = 0;
 
+const isMobileDevice =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 function handler() {
   clickCount += 1;
-  if (clickCount === 1) {
-    btn.innerHTML = 'بیشتر تلاش کن :)';
-  } else if (clickCount === 2) {
-    btn.innerHTML = 'آفرین یبار دیگه :)';
-  } else if (clickCount === 3) {
-    btn.innerHTML = 'تلاش کن تو میتونی :)';
-  } else if (clickCount === 4) {
-    btn.innerHTML = 'خیلی نزدیک شدی :)';
-  } else if (clickCount === 5) {
-    window.location.replace('./../../soon.html');
+  if (isMobileDevice) {
+    btn.innerHTML = 'انگشتت رو روم نگه دار :)';
+    setTimeout(redirectToPage, 5000);
+  } else {
+    if (clickCount === 1) {
+      btn.innerHTML = 'بیشتر تلاش کن :)';
+    } else if (clickCount === 2) {
+      btn.innerHTML = 'آفرین یبار دیگه :)';
+    } else if (clickCount === 3) {
+      btn.innerHTML = 'تلاش کن تو میتونی :)';
+    } else if (clickCount === 4) {
+      btn.innerHTML = 'خیلی نزدیک شدی :)';
+    } else if (clickCount === 5) {
+      redirectToPage();
+    }
   }
+}
+
+function redirectToPage() {
+  window.location.replace('./../../soon.html');
 }
 
 function menu(x) {
